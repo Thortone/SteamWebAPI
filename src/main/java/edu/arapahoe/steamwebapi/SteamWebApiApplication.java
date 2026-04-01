0f48b49e373e02bedc35b6b89bf4276c77eec843
Thortone <mhthor68@gmail.com>
2026-04-01 11:41:43 -0600

    Initial commit

---
+++ src/main/java/edu/arapahoe/steamwebapi/SteamWebApiApplication.java
@@ -0,0 +1,13 @@
+package edu.arapahoe.steamwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamWebApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/arapahoe/steamwebapi/SteamWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.arapahoe.steamwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
